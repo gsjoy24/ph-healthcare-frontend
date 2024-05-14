a228574c3975f6ab79fcfe29da0330fb1ec2648f
gsjoy24 <gour.joy24@gmail.com>
2024-05-14 15:51:24 +0600

    first commit

---
+++ src/lib/Providers/Providers.tsx
@@ -0,0 +1,9 @@
+'use client';
+import { ThemeProvider } from '@mui/material';
+import theme from '../theme/theme';
+
+const Providers = ({ children }: { children: React.ReactNode }) => {
+	return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+};
+
+export default Providers;
