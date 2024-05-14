657f3365062d42e6c08a9f88100ddd208c294085
gsjoy24 <gour.joy24@gmail.com>
2024-05-15 01:48:26 +0600

    feat: Add patient registration page
    - Create a new component for the patient registration page
    - Implement the UI layout and form fields for name, email, password, contact number, and address
    - Add a register button to submit the form
    - Include a link to the login page for users who already have an account

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,8 +1,79 @@
+import assets from '@/assets';
+import { Box, Button, Container, Grid, Stack, TextField, Typography } from '@mui/material';
+import Image from 'next/image';
+import Link from 'next/link';
+
 const RegisterPage = () => {
 	return (
-		<div>
-			<h1>This is RegisterPage component</h1>
-		</div>
+		<Container>
+			<Stack justifyContent='center' alignItems='center'>
+				<Box
+					sx={{
+						maxWidth: '600px',
+						width: '100%',
+						boxShadow: 1,
+						borderRadius: 1,
+						p: 4,
+						mt: 4
+					}}
+				>
+					<Stack
+						sx={{
+							justifyContent: 'center',
+							alignItems: 'center',
+							gap: 2
+						}}
+					>
+						<Box>
+							<Image src={assets.svgs.logo} alt='Ph healthcare logo' width={50} />
+						</Box>
+						<Box>
+							<Typography variant='h6'>Patient Register</Typography>
+						</Box>
+					</Stack>
+
+					<Box>
+						<form>
+							<Grid container spacing={2} my={1}>
+								<Grid item md={12} sm={12}>
+									<TextField label='Name' variant='outlined' size='small' fullWidth />
+								</Grid>
+								<Grid item md={6}>
+									<TextField label='Email' variant='outlined' size='small' fullWidth />
+								</Grid>
+								<Grid item md={6}>
+									<TextField label='Password' type='password' variant='outlined' size='small' fullWidth />
+								</Grid>
+								<Grid item md={6}>
+									<TextField label='Contact Number' type='tel' variant='outlined' size='small' fullWidth />
+								</Grid>
+								<Grid item md={6}>
+									<TextField label='Address' variant='outlined' size='small' fullWidth />
+								</Grid>
+							</Grid>
+
+							<Button
+								sx={{
+									my: 2
+								}}
+								fullWidth
+							>
+								Register
+							</Button>
+
+							<Box>
+								<Typography variant='body2' align='center'>
+									Already have an account?
+									<Button color='primary' variant='text' component={Link} href='/login'>
+										Login
+									</Button>
+								</Typography>
+							</Box>
+						</form>
+					</Box>
+				</Box>
+			</Stack>
+		</Container>
 	);
 };
 
