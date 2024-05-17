c93943213d017e3c27661c344f42b4cc9459e373
gsjoy24 <gour.joy24@gmail.com>
2024-05-17 23:18:47 +0600

    updated

--- src/lib/Providers/Providers.tsx
+++ src/lib/Providers/Providers.tsx
@@ -1,9 +1,15 @@
 'use client';
 import { ThemeProvider } from '@mui/material';
+import { Provider } from 'react-redux';
 import theme from '../theme/theme';
+import { store } from '@/redux/store';
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
-	return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+	return (
+		<Provider store={store}>
+			<ThemeProvider theme={theme}>{children}</ThemeProvider>
+		</Provider>
+	);
 };
 
 export default Providers;
---
+++ src/redux/api/baseApi.ts
@@ -0,0 +1,17 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import type { Pokemon } from './types';
+
+// Define a service using a base URL and expected endpoints
+export const pokemonApi = createApi({
+	reducerPath: 'pokemonApi',
+	baseQuery: fetchBaseQuery({ baseUrl: 'https://pokeapi.co/api/v2/' }),
+	endpoints: (builder) => ({
+		getPokemonByName: builder.query<Pokemon, string>({
+			query: (name) => `pokemon/${name}`
+		})
+	})
+});
+
+// Export hooks for usage in functional components, which are
+// auto-generated based on the defined endpoints
+export const { useGetPokemonByNameQuery } = pokemonApi;
