7572f34a6261acd05904acd5b3e0926c072127be
gsjoy24 <gour.joy24@gmail.com>
2024-05-18 16:51:44 +0600

    added redux

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import {reducer} from './rootReducer';
+import { baseApi } from './api/baseApi';
+import { reducer } from './rootReducer';
 
 export const store = configureStore({
-	reducer
+	reducer,
+	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(baseApi.middleware)
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
